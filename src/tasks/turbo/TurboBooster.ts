58aac4e6a0375e22a4bf97642c58a4e358fd754f
cryptickoan <me@cryptickoan.com>
2022-04-07 09:45:13 -0600

    add whitelist-strategy task
    #none

--- src/tasks/turbo/TurboBooster.ts
+++ src/tasks/turbo/TurboBooster.ts
@@ -1,6 +1,10 @@
 import '@nomiclabs/hardhat-ethers';
+import { ContractFactory } from 'ethers';
+import { parseEther } from 'ethers/lib/utils';
 import { task } from 'hardhat/config';
-import { createTurboBooster } from './utils/turboContracts';
+import { impersonateAccount } from '../../utils/impersonate';
+import { TurboAddresses } from './utils/constants';
+import { createFuseVaultFactory, createTurboBooster } from './utils/turboContracts';
 
 
 /*///////////////////////////////////////////////////////////////
@@ -22,4 +26,41 @@ task('get-is-frozen', "Will return true if boosting for all safes under master i
     const boostableVaults = await turboBoosterContract.frozen()
 
     console.log({boostableVaults})
+})
+
+/*///////////////////////////////////////////////////////////////
+                        METHOD CALLS
+//////////////////////////////////////////////////////////////*/
+
+task('whitelist-strategy', "Will deploy a Fuse4626 vault and list it in the TurboBooster")
+    .addParam('ctoken', 'CToken address that the FuseERC4626 will wrap')
+    .addParam('name', 'Name of the vaults shares/token')
+    .addParam('symbol', 'Symbol of the vaults shares/token')
+    .addParam('id', 'ChainID')
+    .setAction(async (taskArgs, hre) => {
+
+        // 1. Deploy new FuseERC4626
+        const vaultFactory = createFuseVaultFactory(hre.ethers.provider.getSigner())
+        const receipt: any = await vaultFactory.deploy(taskArgs.ctoken, taskArgs.name, taskArgs.symbol)
+
+        // 2. Add a boost cap for vault
+            // Only those with TURBO_ADMIN_ROLE/4 can set booscaps for strategies
+            // The FEI_DAO_TIMELOCK was granted this role.
+
+            // 2.1 seed Fei DAO Timelock
+            const signers = await hre.ethers.getSigners()
+            const transactionHash = await signers[0].sendTransaction({
+                to: TurboAddresses[taskArgs.id].FEI_DAO_TIMELOCK,
+                value: hre.ethers.utils.parseEther("10"), // Sends exactly 1.0 ether
+            });
+
+            console.log(transactionHash)
+
+            // 2.2 Impersonate the Fei Dao Timelock and setBoostCapForVault
+            await impersonateAccount(hre.ethers.provider, TurboAddresses[taskArgs.id].FEI_DAO_TIMELOCK)
+            const signer = await hre.ethers.getSigner(TurboAddresses[taskArgs.id].FEI_DAO_TIMELOCK)
+            const turboBoosterContract = await createTurboBooster(signer, 1)
+            const receipt2 = await turboBoosterContract.setBoostCapForVault(receipt.deployTransaction.creates, parseEther("2000000"))
+
+            console.log({receipt2})
 })
